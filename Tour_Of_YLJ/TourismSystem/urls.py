dc7d387c8eedab2f3524a0b8a83f8d91f1225e1a
NARUTO <2159302122@qq.com>
2025-03-30 01:07:49 +0800

    css无法发挥作用，logout的图标没有显示，前端基本完成，后续主要是完成具体功能和图片的插入

--- Tour_Of_YLJ/TourismSystem/urls.py
+++ Tour_Of_YLJ/TourismSystem/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path("", views.login, name="login"),
-     path("index", views.index, name="index"),
      path("attractions", views.attractions, name="attractions"),
-     path("log", views.login, name="log"),
+     path("log", views.log, name="log"),
      path("mine", views.mine, name="mine"),
-]    
+     path("", views.index, name="index"),
+]
