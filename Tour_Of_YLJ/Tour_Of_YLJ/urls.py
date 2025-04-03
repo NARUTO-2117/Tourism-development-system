2c128c8ee62e2bfb634cd480a35e11099d90dfe1
NARUTO <2159302122@qq.com>
2025-04-03 13:43:22 +0800

    css正常工作

--- Tour_Of_YLJ/Tour_Of_YLJ/urls.py
+++ Tour_Of_YLJ/Tour_Of_YLJ/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('',include("users.urls")),
     path('admin/', admin.site.urls),
-    path('users/',include("users.urls")),
+    # path('users/',include("users.urls")),
     path('TourismSystem/', include("TourismSystem.urls")),
 ]
